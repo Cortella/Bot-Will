288a7fde386e9db05a1b5608b800967220c5ebaf
Cortella <nicolau.cortella@gmail.com>
2020-01-30 10:23:23 -0300

    Adicionando funcoes de click, e mapeamento de tela

--- controller.py
+++ controller.py
@@ -2,6 +2,42 @@
 """
 Created on Wed Jan 29 20:28:39 2020
 
-@author: bnmac
+@author: cortella
 """
 
+#define ID cortellapedras
+#define senha Dest@quemito10
+
+
+#importa biblioteca de controle de maquina
+import pyautogui
+
+#importa biblioteca de temporazicao
+import time
+
+#mapeia tela em pixels e convente em coordenadas cartesianas
+x , y = pyautogui.size()
+
+#get coordenadas do mouse (pixels)
+mouseP1 , mouseP2 = pyautogui.position()
+print(mouseP1)
+print(mouseP2)
+
+time.sleep(2)
+
+pyautogui.moveTo(1009,750)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(810,621)
+time.sleep(1)
+pyautogui.click()
+
+time.sleep(1)
+pyautogui.moveTo(713,346)
+pyautogui.click()
+pyautogui.write("cortellapedras")
+
+#810,621
+#713,346
---
+++ getPoints.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 21:30:46 2020
+
+@author: cortella
+"""
+#GET POINTS
+
+import pyautogui
+
+x,y = pyautogui.size()
