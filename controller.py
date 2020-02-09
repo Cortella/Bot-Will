bea771a0ef673cd76738f826bc887cb4b6961ab0
Cortella <nicolau.cortella@gmail.com>
2020-02-09 04:37:24 -0300

    FIM README

--- controller.py
+++ controller.py
@@ -5,6 +5,8 @@ Created on Wed Jan 29 20:28:39 2020
 @author: cortella
 """
 
+versao = 1.0
+print"BEM VINDO A VERSAO",versao,"DO BOT-FARM
 #DEIXE O APLICATIVO NA TELA DE SELECÇÃO DE SERVIDORES
 #V1.0 -> feito para telas de resolução 1366x768
 #configure o aplicativo para ocupar toda a tela antes de rodar
